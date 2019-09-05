43c49c57508ac7f2ba8931564e36e54ed9d31b45
Subeg7 <Durs1966@rhyta.com>
2019-09-06 00:32:28 +0545

    first run of app

---
+++ src/main/java/com/example/SpringMvc/SpringMvcMain.java
@@ -0,0 +1,14 @@
+
+package com.example.SpringMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class SpringMvcMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+
+}
