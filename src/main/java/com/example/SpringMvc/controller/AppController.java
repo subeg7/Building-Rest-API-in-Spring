01f6db871ae42256409ec9159202b8bb88d713fd
Subeg7 <subeg7@gmail.com>
2019-09-07 23:19:12 +0545

    rest controller

--- src/main/java/com/example/SpringMvc/controller/AppController.java
+++ src/main/java/com/example/SpringMvc/controller/AppController.java
@@ -2,6 +2,7 @@
 package com.example.SpringMvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,6 +18,13 @@ public class AppController{
                 ModelAndView model = new ModelAndView("test");
                 model.addObject("textData","Inventions that changed the world");
                 return model;
+	}
+        
+        @GetMapping("/form")
+	public ModelAndView GetFormView() {
+                ModelAndView model = new ModelAndView("test");
+                model.addObject("textData","Form Page");
+               return model;
 	}
 
 }
--- src/main/java/com/example/SpringMvc/controller/BlogController.java
+++ src/main/java/com/example/SpringMvc/controller/BlogController.java
@@ -12,13 +12,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-import java.util.List;
+import java.util.*;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+//import java.util.Map;
+
 
 /**
  *
  * @author Acerimport java.util.List;
 
  */
+@RestController
 public class BlogController {
     
     @Autowired
@@ -27,6 +34,20 @@ public class BlogController {
     @GetMapping("/blog")
     public List<Blog> index(){
         return blogRepository.findAll();
+    }
+    
+    @GetMapping("/blog/{id}")
+    public Optional<Blog> show(@PathVariable String id){
+        int blogId = Integer.parseInt(id);
+        Blog blog  = new Blog(id+":A small journey","... better be a spring season");
+        return blogRepository.findById(blogId);
+    }
+    
+    @PostMapping("/blog")
+    public Blog create(@RequestBody Map<String, String> body){
+        String title = body.get("title");
+        String content = body.get("content");
+        return blogRespository.save(new Blog(title, content));
     }
     
     
