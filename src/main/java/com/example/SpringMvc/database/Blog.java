ee80e5d5a22f0b946d887b49bebdba1e1d251c59
Subeg7 <subeg7@gmail.com>
2019-09-10 22:28:42 +0545

    Blog Repository and Entity

---
+++ src/main/java/com/example/SpringMvc/database/Blog.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.SpringMvc.database;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Acer
+ */
+@Entity
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    private String title;
+    private String content;
+
+   
+    
+    public Blog(){
+        
+    }
+    
+    public Blog(String title,String content){
+       this.setTitle(title);      
+       this.setContent(content);
+    }
+    
+     public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+    
+    
+
+   
+    
+    
+    
+    
+}
